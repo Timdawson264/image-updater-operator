64a61610ed94e804c8c4d795520fce3706c4f418
Tim Dawson <tidawson@redhat.com>
2022-03-16 01:35:35 +1300

    Working kind of, will crash on a few edge cases

--- README.md
+++ README.md
@@ -4,10 +4,12 @@ This operator is a super simple way to automatically keep deployments up to date
 The goal is to eventually have an annotation to link a deployment to a `Imagebuilder` CRD (from the simple-image-builder operator)  to allow new builds to trigger image updates. This will efectivly allow for a primative replacement of `imagestreams` without the massive ammount of integration normally required.
 
 ## Annotations
-an example to check regsitery every hour, for new image version
+An example to check regsitery every two hours, for new image version
 
-`image-updater.k8s.tdawson.co.nz/enable: true`
-`image-updater.k8s.tdawson.co.nz/watch-cron: "0 * * * *"`
+`image-updater.k8s.tdawson.co.nz/enable: "true"`
+`image-updater.k8s.tdawson.co.nz/period: "3600"` 
+
+
 
 This annotations is set by operator when first run  red_name is from the image propertie of each container this must not be a short name!
 This can also be set by the user to hardcode which image version is used to keep update to date.
@@ -15,5 +17,11 @@ This can also be set by the user to hardcode which image version is used to keep
 `image-updater.k8s.tdawson.co.nz/watch-ref: { "Container-Name": "ref_name", "Container2-name": "ref_name" }`
 
 
+### TODO:
 
-
+- Add helm chart to deploy, with service account + role bindings
+- replace `sched.scheduler` with a custom implimentation that allows easier removal by key, and also adding a new earlier class while waiting for next.
+- implement removing class from scheduler on delete/removal or disalbe of annotation.
+- add support for daemonset
+- rewrite the maze..... of functions
+- add support from namespacing this controller to manage a single namespace.
--- operator.py
+++ operator.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-from kubernetes import client, config
+from kubernetes import client, config, watch
+from kubernetes.leaderelection import leaderelection
+from kubernetes.leaderelection.resourcelock.configmaplock import ConfigMapLock
+from kubernetes.leaderelection import electionconfig
+
 from docker_registry_client import DockerRegistryClient
 import logging
 from pprint import pprint
@@ -8,10 +12,25 @@ from urllib.parse import urlparse
 import sys
 import subprocess
 import json
+import uuid
+import sched
+import time
+import threading
 
 OP_NAME = "image-updater-operator"
+DEFAULT_PERIOD = int( 3600 )
 annotation_enable = 'image-updater.k8s.tdawson.co.nz/enable'
 annotations_wref = 'image-updater.k8s.tdawson.co.nz/watch-ref'
+annotations_status = 'image-updater.k8s.tdawson.co.nz/status'
+annotations_period = 'image-updater.k8s.tdawson.co.nz/period'
+
+scheduler = sched.scheduler(time.time, time.sleep)
+
+#key is gen_key output - used to find entry in schedular
+#watched_resources = dict()
+
+def gen_key( input ):
+        return  "key_" + input.kind + "_"+ input.metadata.namespace + "_" + input.metadata.name
 
 #TODO: replace this with a request to the api
 def get_ref_digest( image_str ):
@@ -23,6 +42,8 @@ def get_ref_digest( image_str ):
     output = json.loads( popen.stdout.read() )
     
     return (output["Digest"])    
+
+
 
 #Returns a dict of all containers: images from the supplied deplyment/daemonset.
 #image returned is the desired ref to watch - aka the original refs.
@@ -38,7 +59,7 @@ def get_container_watch_refs( input ):
         images = dict()
         for cont in containers:
             if  "sha256" in cont.image:
-                logger.error(f"Container {cont.name} in deployment {input.metadata.name} already using sha256 image-ref")
+                logger.error(f"Container {cont.name} in deployment {input.metadata.name} already using sha256 image-ref, You may need to manually set the watch-ref annotation")
                 continue
             images[cont.name] = cont.image
         return images
@@ -82,7 +103,6 @@ def check_image_update( client, deploy ):
                 field_manager=OP_NAME, 
                 field_validation="Strict"
             )
-        #print( resp )
 
 
 def set_watch_ref_annotation( client, deploy ):
@@ -104,27 +124,150 @@ def set_watch_ref_annotation( client, deploy ):
             field_manager=OP_NAME, 
             field_validation="Strict"
         )
+
+def set_update_status_annotation( client, deploy ):
+    deploy_name = deploy.metadata.name
+    deploy_ns = deploy.metadata.namespace
+
+    status = { 
+        "last-checked": int(time.time()) 
+    }
+    status_json = json.dumps(status)
+
+    patch = {
+        "metadata": { 
+            "annotations": { 
+                    annotations_status: status_json 
+                } 
+            }
+        }
         
+    #TODO: wrap in try execpt
+    resp = client.AppsV1Api().patch_namespaced_deployment( 
+            deploy_name, 
+            deploy_ns, 
+            patch,
+            pretty=True, 
+            field_manager=OP_NAME, 
+            field_validation="Strict"
+        )
+
+def get_status_annotation( deploy ):
+    raw_annotation = deploy.metadata.annotations[ annotations_status ]
+    return json.loads( raw_annotation  )
+
+def get_check_period( deploy ):
+    if annotations_period in deploy.metadata.annotations:
+        return int( deploy.metadata.annotations[ annotations_period ] )
+    else:
+        return DEFAULT_PERIOD
+
+def get_last_check( deploy ):
+
+    if annotations_status in deploy.metadata.annotations:
+        raw_annotation = deploy.metadata.annotations[ annotations_status ] 
+        #pprint( raw_annotation )
+        j = json.loads( raw_annotation  )
+        last = int( j[ "last-checked" ] )
+        return last
+    
+    return 0
+
+def get_and_reconsile( kind, namespace, name ):
+    print( f"Scheduled update ts: {time.time()} {kind} {namespace}/{name}")
+    if kind == "Deployment":
+        #TODO: Catch exections from removed objeccts
+        obj = client.AppsV1Api().read_namespaced_deployment( name, namespace )
+        if annotation_enable in obj.metadata.annotations and obj.metadata.annotations[annotation_enable].lower() == "true":
+            set_update_status_annotation( client, obj )
+            check_image_update( client, obj )
+            period = get_check_period( obj )
+            scheduler.enter( period , 5, get_and_reconsile, ( kind, namespace, name ) )
+    
+def reconsile_add(obj):
+    #this has no side effects
+    set_watch_ref_annotation( client, obj )
+
+    last = get_last_check( obj )
+    now = int(time.time())
+    period = get_check_period( obj )
+    print( f"time until update: { period - (now-last) }" ) 
+
+    if (now - last) >  period:
+        #update status, aka last check time first, this means that any following update events contain the new time.
+        set_update_status_annotation( client, obj )
+        check_image_update( client, obj )
+    else:
+        period = period - (now-last)
+
+    kind = obj.kind
+    ns = obj.metadata.namespace
+    name = obj.metadata.name
+
+    
+    scheduler.enter( period , 5, get_and_reconsile, ( kind,ns,name ) )
+
+#when we add more watches https://engineering.bitnami.com/articles/kubernetes-async-watches.html
+def start_controller():
+    api_client = client.AppsV1Api()
+
+    w = watch.Watch()
+    for event in w.stream(api_client.list_deployment_for_all_namespaces):
+        print("Event: %s %s %s" % (event['type'], event['object'].kind, event['object'].metadata.name))
+        #pprint( event )
+        d = event['object']
+        key = gen_key( event['object'] )
+
+        if annotation_enable in d.metadata.annotations and d.metadata.annotations[annotation_enable].lower() == "true":
+            if event['type'] in ["ADDED"]:
+                reconsile_add( d )
+            #if DELETED:
+            # remove from sched
+
+
+        if event['type'] in ["MODIFIED"]:
+            pass
+            #TODO check if enable annotation removed.
+
+
+    sys.exit("Watch failed")
+
+
+def stop_controller():
+    logger.warn("No longer the leader")
+    sys.exit(1)
 
 def main():
+    #TODO: add some arg parseing 
+    # --global = check all namespace
+    # --namespace = check only this namespace - also we should be running in this names - leader election...
+    # --in-cluster = use svc account and in cluster env config.
+
     #Use Service Account
     #config.load_incluster_config()
     config.load_config()
 
-    #TODO: add a leader election
+    #Leader election
+    # candidate_id = uuid.uuid4()
+    # lock_namespace = "image-updater-operator" #TODO: fix this - maybe arg/maybe api call (configmap/current svc acount ns)
+    # lock_name = "image-updater-operator-leader" #Could also be from a configmap/arg
 
-    deployments = client.AppsV1Api().list_deployment_for_all_namespaces(
-        async_req=False
-    )
+    # leader = electionconfig.Config(ConfigMapLock(lock_name, lock_namespace, candidate_id), lease_duration=17,
+    #                            renew_deadline=11, retry_period=5, onstarted_leading=start_controller,
+    #                            onstopped_leading=stop_controller)
 
-    #Select only deployments that are annotated with enabled=true
-    filter_func =  lambda d: annotation_enable in d.metadata.annotations and d.metadata.annotations[annotation_enable].lower() == "true"
-    deployments_enabled = filter( filter_func , deployments.items )
+    # leaderelection.LeaderElection(leader).run()
 
-    for deploy in deployments_enabled:
-        set_watch_ref_annotation( client, deploy )
-        check_image_update( client, deploy )
-        
+    #HACK: This task just readds its self to the queue to stop the schedular getting empty
+    def dummy():
+        scheduler.enter( 60, 10, dummy  )
+    dummy()
+
+    sched_thread = threading.Thread(target=scheduler.run, daemon=True)
+    sched_thread.start()
+
+    start_controller()
+
 
 if __name__ == '__main__':
     main()
